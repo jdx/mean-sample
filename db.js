00e213120701e861906e5f19e3da0b0c79e86a96
dickeyxxx <jeff@dickeyxxx.com>
2014-06-21 18:58:03 -0700

    stuff

---
+++ db.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/social', function () {
+  console.log('mongodb connected')
+})
+
+module.exports = mongoose
--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var Post = require('./models/post')
 
 var app = express()
 app.use(bodyParser.json())
 
-app.get('/api/posts', function (req, res) {
-  res.json([
-    {
-      username: 'dickeyxxx',
-      body: 'node rocks!'
-    }
-  ])
+app.get('/api/posts', function (req, res, next) {
+  Post.find(function(err, posts) {
+    if (err) { return next(err) }
+    res.json(posts)
+  })
 })
 
-app.post('/api/posts', function (req, res) {
-  console.log('post received!')
-  console.log(req.body.username)
-  console.log(req.body.body)
-  res.send(201)
+app.post('/api/posts', function (req, res, next) {
+  var post = new Post({
+    username: req.body.username,
+    body: req.body.body
+  })
+  post.save(function (err, post) {
+    if (err) { return next(err) }
+    res.json(201, post)
+  })
 })
 
 app.listen(3000, function () {
