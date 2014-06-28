345b34daa1882738a72b88eedf9f9a564c6001b6
dickeyxxx <jeff@dickeyxxx.com>
2014-06-27 17:37:55 -0700

    more gulping

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
+var sourcemaps = require('gulp-sourcemaps')
 var uglify = require('gulp-uglify')
 var ngAnnotate = require('gulp-ng-annotate')
 
 gulp.task('js', function () {
   gulp.src(['ng/module.js', 'ng/**/*.js'])
-    .pipe(ngAnnotate())
-    .pipe(uglify())
-    .pipe(concat('app.js'))
+    .pipe(sourcemaps.init())
+      .pipe(concat('app.js'))
+      .pipe(ngAnnotate())
+      .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('assets'))
 })
 
