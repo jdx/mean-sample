3a4abd85462baa34529fd39345843da30c044c2b
dickeyxxx <jeff@dickeyxxx.com>
2014-07-26 16:58:07 -0700

    reconnectable

--- ng/websocket.js
+++ ng/websocket.js
@@ -1,20 +1,26 @@
 angular.module('app')
-.run(function ($window, $rootScope) {
-  var connection = new $window.WebSocket('ws://localhost:3000')
+.run(function ($window, $rootScope, $timeout) {
+  function connect() {
+    var url = 'ws://localhost:3000'
+    var connection = new $window.WebSocket(url)
 
-  connection.onopen = function (e) {
-    console.log('WebSocket opened')
-    $rootScope.$broadcast('websocket_open', e)
+    connection.onopen = function (e) {
+      console.log('WebSocket opened')
+      $rootScope.$broadcast('websocket_open', e)
+    }
+
+    connection.onclose = function (e) {
+      console.log('WebSocket closed. Reconnecting...')
+      $rootScope.$broadcast('websocket_close', e)
+      $timeout(connect, 10*1000)
+    }
+
+    connection.onmessage = function (e) {
+      console.log('WebSocket message:', e)
+      var payload = JSON.parse(e.data)
+      $rootScope.$broadcast(payload.topic, payload.data)
+    }
   }
 
-  connection.onclose = function (e) {
-    console.log('WebSocket closed')
-    $rootScope.$broadcast('websocket_close', e)
-  }
-
-  connection.onmessage = function (e) {
-    console.log('WebSocket message:', e)
-    var payload = JSON.parse(e.data)
-    $rootScope.$broadcast(payload.topic, payload.data)
-  }
+  connect()
 })
