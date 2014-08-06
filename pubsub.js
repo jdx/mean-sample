9f858de50fd1bee4255384596e28b5d2a76ffe43
dickeyxxx <jeff@dickeyxxx.com>
2014-08-05 23:14:39 -0700

    use redis-url to read redis config from env var

--- pubsub.js
+++ pubsub.js
@@ -1,12 +1,12 @@
-var redis = require('redis')
-var client = redis.createClient()
+var redis = require('redis-url')
+var client = redis.connect()
 
 exports.publish = function (topic, data) {
   client.publish(topic, JSON.stringify(data))
 }
 
 exports.subscribe = function (topic, cb) {
-  var client = redis.createClient()
+  var client = redis.connect()
   client.subscribe(topic)
   client.on('message', function (channel, message) {
     cb(JSON.parse(message))
