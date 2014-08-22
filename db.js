2ef4d1fb63fece086c301a42a78f736d76aac599
dickeyxxx <jeff@dickeyxxx.com>
2014-08-21 20:09:25 -0700

    wercker mongo config

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose')
-var url = process.env.MONGOLAB_URI || 'mongodb://localhost/social'
+var url = process.env.MONGODB_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/social'
 mongoose.connect(url)
 module.exports = mongoose
