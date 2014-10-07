88ea57c81ed1890e86b76a04c0ad59f50b30db3d
dickeyxxx <jeff@dickeyxxx.com>
2014-10-07 14:02:21 -0700

    ch 5: break up angular code into controller + service

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,9 @@
 var app = angular.module('app', [])
 
-app.controller('PostsCtrl', function ($scope, $http) {
+app.controller('PostsCtrl', function ($scope, PostsSvc) {
   $scope.addPost = function () {
     if ($scope.postBody) {
-      $http.post('/api/posts', {
+      PostsSvc.create({
         username: 'dickeyxxx',
         body:     $scope.postBody
       })
@@ -14,8 +14,17 @@ app.controller('PostsCtrl', function ($scope, $http) {
     }
   }
 
-  $http.get('/api/posts')
+  PostsSvc.fetch()
   .success(function (posts) {
     $scope.posts = posts
   })
+})
+
+app.service('PostsSvc', function ($http) {
+  this.fetch = function () {
+    return $http.get('/api/posts')
+  }
+  this.create = function (post) {
+    return $http.post('/api/posts', post)
+  }
 })
