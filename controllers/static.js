9626c2b0e373de8f20ed63c40c0ff85bd2d4296c
dickeyxxx <jeff@dickeyxxx.com>
2014-10-07 16:45:13 -0700

    ch 8: renamed posts layout -> app layout

--- controllers/static.js
+++ controllers/static.js
@@ -6,7 +6,7 @@ router.use(express.static(__dirname + '/../assets'))
 router.get('/', function (req, res) {
   // render/ejs is now easier to use since
   // sendFile has security restrictions on relative pathing
-  res.render('posts.html.ejs')
+  res.render('app.html.ejs')
 })
 
 module.exports = router
