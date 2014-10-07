3be8afa190610d22e665a6963dd5af2d77d252d6
dickeyxxx <jeff@dickeyxxx.com>
2014-10-07 14:30:17 -0700

    ch 6: module gulp

---
+++ gulp/scripts.js
@@ -0,0 +1,19 @@
+var gulp   = require('gulp')
+var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
+var ngAnnotate = require('gulp-ng-annotate')
+var sourcemaps = require('gulp-sourcemaps')
+
+gulp.task('js', function () {
+  return gulp.src(['ng/module.js', 'ng/**/*.js'])
+    .pipe(sourcemaps.init())
+      .pipe(concat('app.js'))
+      .pipe(ngAnnotate())
+      .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('assets'))
+})
+
+gulp.task('watch:js', ['js'], function () {
+  gulp.watch('ng/**/*.js', ['js'])
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,8 @@
-var gulp   = require('gulp')
-var concat = require('gulp-concat')
-var uglify = require('gulp-uglify')
-var ngAnnotate = require('gulp-ng-annotate')
-var sourcemaps = require('gulp-sourcemaps')
-
-gulp.task('js', function () {
-  return gulp.src(['ng/module.js', 'ng/**/*.js'])
-    .pipe(sourcemaps.init())
-      .pipe(concat('app.js'))
-      .pipe(ngAnnotate())
-      .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('assets'))
+var gulp = require('gulp')
+var fs   = require('fs')
+fs.readdirSync(__dirname + '/gulp').forEach(function (task) {
+  require('./gulp/' + task)
 })
 
-gulp.task('watch:js', ['js'], function () {
-  gulp.watch('ng/**/*.js', ['js'])
-})
+gulp.task('build', ['js'])
+gulp.task('watch', ['watch:js'])
