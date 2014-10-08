c17c9e83ce0248f13cea843afd1bb0b8102f09db
dickeyxxx <jeff@dickeyxxx.com>
2014-10-07 17:54:44 -0700

    ch 9: redirect after login/register

--- ng/login.ctrl.js
+++ ng/login.ctrl.js
@@ -1,9 +1,10 @@
 angular.module('app')
-.controller('LoginCtrl', function ($scope, UserSvc) {
+.controller('LoginCtrl', function ($scope, UserSvc, $location) {
   $scope.login = function (username, password) {
     UserSvc.login(username, password)
     .then(function (user) {
       $scope.$emit('login', user)
+      $location.path('/')
     })
   }
 })
--- ng/register.ctrl.js
+++ ng/register.ctrl.js
@@ -1,9 +1,10 @@
 angular.module('app')
-.controller('RegisterCtrl', function ($scope, UserSvc) {
+.controller('RegisterCtrl', function ($scope, UserSvc, $location) {
   $scope.register = function (username, password) {
     UserSvc.register(username, password)
     .then(function (user) {
       $scope.$emit('login', user)
+      $location.path('/')
     })
   }
 })
