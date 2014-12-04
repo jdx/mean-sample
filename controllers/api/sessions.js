cf0d8c3bdbefc80bd9fa70062b2dc23c4435ed82
Jeff Dickey <jeff@dickeyxxx.com>
2014-12-04 15:48:44 -0800

    Update sessions.js

--- controllers/api/sessions.js
+++ controllers/api/sessions.js
@@ -4,6 +4,9 @@ var jwt    = require('jwt-simple')
 var config = require('../../config')
 var User   = require('../../models/user')
 
+// Note that the book had this as router.post('/' but had this router mounted at '/api/sessions'.
+// In this code it's mounted at '/api' and is router.post('/sessions'
+// Either way will be fine, but the full URL should be '/api/sessions'
 router.post('/sessions', function (req, res, next) {
   var username = req.body.username
   User.findOne({username: username})
